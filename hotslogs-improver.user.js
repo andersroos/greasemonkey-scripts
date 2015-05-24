27d45b408798e97248de03261a797d78281aff2b
Anders Roos <anders.roos@gmail.com>
2015-05-24 14:51:50 +0200

    Added removal of hero level.

--- hotslogs-improver.user.js
+++ hotslogs-improver.user.js
@@ -9,16 +9,25 @@
 $(function() {
     var hero_table = $('#ctl00_MainContent_RadGridCharacterStatistics_ctl00');
     
-    // Filter out when less than 10 games played:
-    
     var even = true;
+
+    // Remove hero level.
+    var th = hero_table.find('th:nth-child(4n)');
+    th.hide();
+
     hero_table.find('tbody tr').each(function(index, element) {
         var tr = $(element);
+        
         var td = tr.find('td:nth-child(5n)');
         if (parseInt(td.text()) < 10) {
+            // Remove heroes with less that 10 games.
             tr.hide();
         }
         else {
+            // Remove hero level.
+            var td = tr.find('td:nth-child(4n)');
+            td.hide();
+            
             if (even) {
                 tr.removeClass("rgRow rgAltRow").addClass("rgRow");
             }
